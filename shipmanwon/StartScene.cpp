a90348e237cc8f3ab42ba1c1cfa898b602a529c9
jsfumato <jsfumato@naver.com>
2016-05-16 11:43:48 +0900

    주석 제거

--- shipmanwon/StartScene.cpp
+++ shipmanwon/StartScene.cpp
@@ -8,16 +8,8 @@ void CStartScene::initialize()
 {
 	setSceneColor(0.2f, 0.0f, 0.0f, 1.0f);
 
-	//CMyObject* stage = new CMyObject();
-
-	//CStageObject* stage2 = new CStageObject();
-	//CMyObject* stage2 = new CStageObject(); //test
-	
 	CPlayerObject* object = new CPlayerObject();
 
 	pushBack(object, 10);
 	object->setScale(0.5f, 0.2f, 0.5f);
-
-	//object1->setTranslate(3.0f, 0.0f, 0.0f);
-	//object1->setScale(0.5f, 0.5f, 0.5f);
 }

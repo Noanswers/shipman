f27f968dbef3576e67e81c4ca2ab976cc97f1573
inho9377 <inho9377@naver.com>
2016-05-23 21:24:44 +0900

    사운드클래스추가

---
+++ shipmanwon/Sound.h
@@ -0,0 +1,61 @@
+/////////////
+// LINKING //
+/////////////
+#pragma comment(lib, "dsound.lib")
+#pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "winmm.lib")
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <windows.h>
+#include <mmsystem.h>
+#include <dsound.h>
+#include <stdio.h>
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Class name: SoundClass
+///////////////////////////////////////////////////////////////////////////////
+class CSound
+{
+private:
+	struct WaveHeaderType
+	{
+		char chunkId[4];
+		unsigned long chunkSize;
+		char format[4];
+		char subChunkId[4];
+		unsigned long subChunkSize;
+		unsigned short audioFormat;
+		unsigned short numChannels;
+		unsigned long sampleRate;
+		unsigned long bytesPerSecond;
+		unsigned short blockAlign;
+		unsigned short bitsPerSample;
+		char dataChunkId[4];
+		unsigned long dataSize;
+	};
+
+public:
+	CSound();
+	CSound(const CSound&);
+	~CSound();
+	bool Initialize(HWND);
+	void Shutdown();
+
+private:
+	bool InitializeDirectSound(HWND);
+	void ShutdownDirectSound();
+
+	bool LoadWaveFile(char*, IDirectSoundBuffer8**);
+	void ShutdownWaveFile(IDirectSoundBuffer8**);
+
+	bool PlayWaveFile();
+
+private:
+	IDirectSound8* m_DirectSound;
+	IDirectSoundBuffer* m_primaryBuffer;
+	IDirectSoundBuffer8* m_secondaryBuffer1;
+};
